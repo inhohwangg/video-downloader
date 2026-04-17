736376a4413f6d0118384d4d60766446b227776a
Inho hwang (황인호) <101077035+inhohwangg@users.noreply.github.com>
2026-04-17 23:29:21 +0900

    Add app/src/main/java/com/inhohwangg/videodownloader/ui/theme/Color.kt

---
+++ app/src/main/java/com/inhohwangg/videodownloader/ui/theme/Color.kt
@@ -0,0 +1,16 @@
+package com.inhohwangg.videodownloader.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val TossPrimary     = Color(0xFF3182F6)
+val TossPrimaryDark = Color(0xFF1B64DA)
+val TossBackground  = Color(0xFFF9FAFB)
+val TossCardBg      = Color(0xFFFFFFFF)
+val TossTextPrimary = Color(0xFF191F28)
+val TossTextSecondary = Color(0xFF8B95A1)
+val TossTextHint    = Color(0xFFB0B8C1)
+val TossSuccess     = Color(0xFF00C471)
+val TossError       = Color(0xFFFF4D4F)
+val TossWarning     = Color(0xFFFFB020)
+val TossDivider     = Color(0xFFF2F4F6)
+val TossSurface     = Color(0xFFF2F4F6)
